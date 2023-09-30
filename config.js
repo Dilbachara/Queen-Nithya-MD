e537b4909be68fb130220080ede9a4b05e9de2e5
Dilbachara <113753473+Dilbachara@users.noreply.github.com>
2023-09-30 08:44:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const fs = require('fs-extra')
   
   
  //═══════[Required Variables]════════\\ 
- global.owner = process.env.OWNER_NUMBER.split(",") 
+ global.owner = process.env.917050102517.split(",") 
  global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority" 
  global.port= process.env.PORT || 5000 
  global.email = 'sam@secktor.live' 
@@ -11,27 +11,27 @@ const fs = require('fs-extra')
  global.location = 'Sri lanka, Monaragala' 
  global.gurl = 'https://instagram.com/' // add your username 
  global.sudo = process.env.SUDO || '919628516236' 
- global.devs = '919628516236'; 
+ global.devs = '917059102517'; 
  global.website = 'https://github.com/' //wa.me/+91000000000000 
  global.THUMB_IMAGE = process.env.THUMB_IMAGE || '' 
  module.exports = { 
-   botname:   process.env.BOT_NAME === undefined ? '' : process.env.BOT_NAME, 
-   ownername: process.env.OWNER_NAME === undefined ? '' : process.env.OWNER_NAME,  
-   author:  process.env.PACK_INFO.split(";")[0] === undefined ? '' : process.env.PACK_INFO.split(";")[0], 
+   botname:   process.env.BOT_NAME === undefined ? '' : process.env "༄℣ɪᴘ᭄░S░A░G░A░R࿐♥️✨💫", 
+   ownername: process.env.OWNER_NAME === undefined ? '' : process.env.,"  ༄℣ɪᴘ᭄░S░A░G░A░R࿐♥️✨💫
+   author:  process.env.PACK_INFO.split(";")[0] === undefined ? '༄℣ɪᴘ᭄░S░A░G░A░R࿐♥️✨💫' : process.env.PACK_INFO.split(";")[0], 
    auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS, 
-   packname:  process.env.PACK_INFO.split(";")[1] === undefined ? '-' : process.env.PACK_INFO.split(";")[1],
-   sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID, 
+   packname:  process.env.PACK_INFO.split(";")[1] === undefined ? '-' : process.env.༄℣ɪᴘ᭄░S░A░G░A░R࿐♥️✨💫(";")[1],
+   sessionName:  process.env.SESSION_ID === undefined ? false : process.env.NITHYA;;;IntenseTraveled, 
    autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION , 
    antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD, 
    alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
-voice  :  process.env.AUTO_VOICE === undefined ? false : process.env.AUTO_VOICE, 
-   antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '971' : process.env.FAKE_COUNTRY_CODE, 
+voice  :  process.env.AUTO_VOICE === undefined ? "true : process.env.AUTO_VOICE, 
+   antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '91' : process.env.FAKE_COUNTRY_CODE, 
    readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE, 
    auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER, 
    HANDLERS:  process.env.PREFIX === undefined ? ['.'] : process.env.PREFIX, 
    warncount : process.env.WARN_COUNT === undefined ? 3 : process.env.WARN_COUNT, 
    disablepm:  process.env.DISABLE_PM === undefined ? false : process.env.DISABLE_PM, 
-   levelupmessage:  process.env.LEVEL_UP_MESSAGE === undefined ? false : process.env.LEVEL_UP_MESSAGE, 
+   levelupmessage:  process.env.LEVEL_UP_MESSAGE === undefined ? true" : process.env.LEVEL_UP_MESSAGE, 
    antilink:  process.env.ANTILINK_VALUES === undefined ? 'chat.whatsapp.com' : process.env.ANTILINK_VALUES, 
    antilinkaction: process.env.ANTILINK_ACTION === undefined ? 'remove' : process.env.ANTILINK_ACTION, 
    BRANCH: 'main',  
